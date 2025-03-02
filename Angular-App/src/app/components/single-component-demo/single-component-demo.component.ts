b0dab4265248d02d9739c33dcf8871f260b97256
viratwoyce <vsathavara@woyce.io>
2025-03-02 23:09:50 +0530

    Data binding part done

---
+++ Angular-App/src/app/components/single-component-demo/single-component-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-component-demo',
+  imports: [],
+  template: "<h1 class='text-primary font-bold text-4xl'>Hello This is Single Component</h1>",
+  styles: ['.text-primary {color: green}']
+})
+export class SingleComponentDemoComponent {
+
+}
