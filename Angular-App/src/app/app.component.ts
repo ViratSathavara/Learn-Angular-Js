ca07c91d1737c2b6b7af78fc49985b3e03e8e0e8
viratwoyce <vsathavara@woyce.io>
2025-03-02 23:22:26 +0530

    add in dev

--- Angular-App/src/app/app.component.ts
+++ Angular-App/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminComponent } from "./components/admin/admin.component";
-import { UserComponent } from "./components/user/user.component";
-import { SingleComponentDemoComponent } from "./components/single-component-demo/single-component-demo.component";
 import { DataBindingComponent } from "./components/data-binding/data-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AdminComponent, UserComponent, SingleComponentDemoComponent, DataBindingComponent],
+  imports: [RouterOutlet, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
